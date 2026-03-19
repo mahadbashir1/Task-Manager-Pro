aff3af02ee5245134d9e7409f91a7f96e2eb76bc
mahadbashir1 <mmahadbashir.01@gmail.com>
2026-03-19 12:17:06 +0500

    fix: resolve javascript syntax error in public directory

--- public/script.js
+++ public/script.js
@@ -263,3 +263,14 @@ function escapeHTML(str) {
         }[tag] || tag)
     );
 }
+
+// Fluid Mouse Hover Effect Tracker
+document.getElementById('tasks-container').onmousemove = e => {
+  for(const card of document.getElementsByClassName('task-card')) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+    card.style.setProperty('--mouse-x', `${x}px`);
+    card.style.setProperty('--mouse-y', `${y}px`);
+  }
+};
